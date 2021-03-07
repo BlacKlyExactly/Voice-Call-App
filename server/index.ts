cb26e312e62c69e99ed991f997501cc02ff46d2d
BlacKlyExactly <supreme24d@gmail.com>
2021-03-07 15:28:28 +0100

    init

--- server/index.ts
+++ server/index.ts
@@ -50,7 +50,7 @@ const peerServer = ExpressPeerServer(httpServer);
 app.use("/rtc", ExpressPeerServer(httpServer))
 
 app.get("/", ( req, res ) => {
-    res.sendFile(path.join(path.resolve("../"), "client", "build", "index.html"));
+    res.sendFile(path.join(path.resolve("../"), "client", "build"));
 });
 
 app.get("/api/rooms", ( req, res ) => res.send({ connections }));
