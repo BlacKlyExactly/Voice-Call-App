447994d3f94230d2f87672015f14835da0e848b9
BlacKlyExactly <supreme24d@gmail.com>
2021-03-08 15:42:58 +0100

    Peer server fix

--- client/src/utils/peer.ts
+++ client/src/utils/peer.ts
@@ -5,7 +5,7 @@ const port: string = process.env.NODE_ENV === 'production' ? "" : "8080"
 const customGenerationFunction = () => (Math.random().toString(36) + '0000000000000000000').substr(2, 16);
 
 const peer = new Peer(customGenerationFunction(), {
-    host: "localhost",
+    host: location.hostname,
     port: port !== "" ? parseInt(port) : 80,
     path: "/rtc",
     debug: 1,
--- client/src/utils/socket.ts
+++ client/src/utils/socket.ts
@@ -2,7 +2,7 @@ import { io, Socket } from "socket.io-client";
 
 const port: string = process.env.NODE_ENV === 'production' ? "" : ":8080"
 
-const socketUrl: string = `http://localhost${port}`;
+const socketUrl: string = `${location.protocol}//${location.hostname}${port}`;
 const socket: Socket =  io(socketUrl);
 
 export default socket;
